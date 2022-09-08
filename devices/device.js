55bda45521fb15a39c5824f47858ff552009b304
lamsd <huynhlam14091994@gmail.com>
2022-09-08 15:34:41 +0700

    upload data V2

---
+++ devices/device.js
@@ -0,0 +1,26 @@
+module.exports = [
+    // Buid-in device
+    'arduinoUno',
+    'arduinoNano',
+    'arduinoLeonardo',
+    'arduinoMega2560',
+    'esp32',
+    'arduinoEsp32',
+    'microPythonEsp32',
+    'esp8266NodeMCU',
+    'arduinoEsp8266NodeMCU',
+    'microPythonEsp8266NodeMCU',
+    'microbit',
+    'microbitV2',
+    'k210MaixDock',
+    'arduinoK210MaixDock',
+    'microPythonK210MaixDock',
+    'k210Maixduino',
+    'arduinoK210Maixduino',
+    'microPythonK210Maixduino',
+    'makeyMakey',
+    // Third party
+    'deviceDemo_esp32',
+    'deviceDemo_arduinoEsp32',
+    'deviceDemo_microPythonEsp32'
+];
